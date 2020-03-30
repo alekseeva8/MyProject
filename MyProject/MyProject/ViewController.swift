70ba2fe91a615d86b2f5e3e30951df00578b2813
alekseeva8 <alekseeva.ev8@gmail.com>
2020-03-30 12:04:21 +0300

    start

---
+++ MyProject/MyProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyProject
+//
+//  Created by Elena Alekseeva on 3/30/20.
+//  Copyright © 2020 Elena Alekseeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
