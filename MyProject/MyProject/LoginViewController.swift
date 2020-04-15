82fadab6d3a8a129ad70ed20dd7cd2bc8624a43b
alekseeva8 <alekseeva.ev8@gmail.com>
2020-04-15 20:44:45 +0300

    first screens, app icon

---
+++ MyProject/MyProject/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  MyProject
+//
+//  Created by Elena Alekseeva on 4/10/20.
+//  Copyright © 2020 Elena Alekseeva. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loginButton.layer.cornerRadius = 5
+
+    }
+
+
+}
