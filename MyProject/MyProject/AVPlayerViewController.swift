26a9705dc9064c11f5708c998ddcb0385aeb7f74
alekseeva8 <alekseeva.ev8@gmail.com>
2020-04-27 20:18:40 +0300

    AVPlayerVC: animation added

--- MyProject/MyProject/AVPlayerViewController.swift
+++ MyProject/MyProject/AVPlayerViewController.swift
@@ -10,21 +10,50 @@ import UIKit
 
 class AVPlayerViewController: UIViewController {
 
+    @IBOutlet weak var reverseView: UIView!
+    @IBOutlet weak var playView: UIView!
+    @IBOutlet weak var forwardView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+
+    @IBOutlet weak var reverseButton: UIButton!
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var forwardButton: UIButton!
+
+    var isPlaying: Bool = true {
+        didSet {
+            if isPlaying {
+                playPauseButton.setImage(UIImage(named: "pause"), for: .normal)
+            }
+            else {
+                playPauseButton.setImage(UIImage(named: "play"), for: .normal)
+            }
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setBackgroundView(reverseView)
+        setBackgroundView(playView)
+        setBackgroundView(forwardView)
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func playPauseButtonTapped(_ sender: UIButton) {
+        isPlaying.toggle()
     }
-    */
 
+    @IBAction func touchedDown(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.3) {
+        sender.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+    }
+    }
+
+    @IBAction func touchedUpInside(_ sender: UIButton) {
+        sender.transform = CGAffineTransform.identity
+    }
+
+    //setting up background views of buttons
+    func setBackgroundView(_ backgroundView: UIView) {
+        backgroundView.layer.cornerRadius = backgroundView.bounds.width/2
+        backgroundView.clipsToBounds = true
+    }
 }
