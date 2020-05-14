6b784c0c0619cdca4c766b86a40453db4e113b23
alekseeva8 <alekseeva.ev8@gmail.com>
2020-05-14 19:25:14 +0300

    Validator added

--- MyProject/MyProject/Initial Screens/SignupViewController.swift
+++ MyProject/MyProject/Initial Screens/SignupViewController.swift
@@ -41,7 +41,21 @@ class SignupViewController: StackViewController {
     }
 
     @objc func buttonTapped(sender: UIButton) {
-        MyUserDefaults.saveSignedValue()
-        performSegue(withIdentifier: "fromSigninToMainVC", sender: nil)
+        let username = usernameTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        let repeatPassword = repeatPasswordTextField.text ?? ""
+
+        //checking the validation of login and password
+        let validator = Validator()
+        if validator.isLoginCorrect(login: username) == true &&
+            validator.isLoginContainsCorrectSymbols(login: username) == true &&
+            validator.isPasswordCorrect(password: password) == true && validator.isRepeatPasswordCorrect(password: password, repeatPassword: repeatPassword) {
+            //saving the fact of user's registration
+            MyUserDefaults.saveSignedValue()
+            performSegue(withIdentifier: "fromSignupToMainVC", sender: nil)
+        } else {
+        }
+
+
     }
 }
