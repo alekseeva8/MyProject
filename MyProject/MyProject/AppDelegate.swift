e837157b67da8c51955e11e30d63333132f307ab
alekseeva8 <alekseeva.ev8@gmail.com>
2020-10-13 11:58:00 +0300

    SceneDelegate deleted

--- MyProject/MyProject/AppDelegate.swift
+++ MyProject/MyProject/AppDelegate.swift
@@ -12,7 +12,15 @@ import Firebase
 @UIApplicationMain
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        let initialVC = Router.setInitialScreen()
+        window?.rootViewController = initialVC
+        window?.makeKeyAndVisible()
+        
         FirebaseApp.configure()
         return true
     }
